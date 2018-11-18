d5ea01e7687af127df57b3c108beada314ab590f
DELL LATITUDE E5480 <DELL LATITUDE E5480@DESKTOP-74RC74P>
2018-11-18 19:57:16 +0100

    hahah

--- WebApplication2/Controllers/HomeController.cs
+++ WebApplication2/Controllers/HomeController.cs
@@ -11,6 +11,7 @@ namespace WebApplication2.Controllers
         public ActionResult Index()
         {
             return View();
+            //hello
         }
 
         public ActionResult About()
